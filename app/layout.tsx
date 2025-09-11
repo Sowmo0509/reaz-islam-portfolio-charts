2e9e6ffd580aeeac9e3f88a717bb8c53777d0fb9
Sowmo0509 <sowmo0509@gmail.com>
2025-09-11 23:54:14 +0600

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-purple-bg`}>
-        <Navbar02 />
-        {children}
+        <Navbar02 />x{children}
       </body>
     </html>
   );
